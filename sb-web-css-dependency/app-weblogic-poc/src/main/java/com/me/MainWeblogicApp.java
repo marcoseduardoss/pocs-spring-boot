4f7e20c1c87bdd1b15e17656776a4261e6ecb3c2
VMX\marcos.eduardo <marcos.eduardo@vmetrix.com>
2022-02-16 18:38:41 -0300

    Add project weblogic project

--- sb-web-css-dependency/app-weblogic-poc/src/main/java/com/me/MainWeblogicApp.java
+++ sb-web-css-dependency/app-weblogic-poc/src/main/java/com/me/MainWeblogicApp.java
@@ -1,16 +1,21 @@
 package com.me;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class ConfigApp extends SpringBootServletInitializer implements WebApplicationInitializer{
+public class MainWeblogicApp extends SpringBootServletInitializer implements WebApplicationInitializer{
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ConfigApp.class);
+		return application.sources(MainWeblogicApp.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MainWeblogicApp.class, args);
 	}
 	
 }
