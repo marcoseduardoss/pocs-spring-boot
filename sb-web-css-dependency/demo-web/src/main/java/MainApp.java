edf4b34941855d2e3d237873edde8b4296b77047
VMX\marcos.eduardo <marcos.eduardo@vmetrix.com>
2022-02-16 14:42:34 -0300

    Add prject sb-web-css-dependency

---
+++ sb-web-css-dependency/demo-web/src/main/java/MainApp.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+
+import com.me.ConfigApp;
+
+public class MainApp{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApp.class, args);
+	}
+
+}
